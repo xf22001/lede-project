44963adf80f94fad87e0c15c4bee2a2722bf7c14
xiaofei <alibaba22001@163.com>
2024-04-26 10:57:54 +0800

    update

--- compile.sh
+++ compile.sh
@@ -6,15 +6,16 @@
 #   文件名称：compile.sh
 #   创 建 者：肖飞
 #   创建日期：2023年07月25日 星期二 10时13分54秒
-#   修改日期：2023年08月14日 星期一 16时01分38秒
+#   修改日期：2024年04月26日 星期五 10时57分27秒
 #   描    述：
 #
 #================================================================
 function main() {
 	./scripts/feeds update -a
 	./scripts/feeds install -a
-	make menuconfig
-	#make -j4 V=s
+	make defconfig
+	#make menuconfig
+	make -j4 V=s
 }
 
 main $@
