bfe40bc670c626cb9546a7782abf3f753ec4ca81
xiaofei <alibaba22001@163.com>
2024-04-26 13:44:03 +0800

    update

--- compile.sh
+++ compile.sh
@@ -6,17 +6,29 @@
 #   文件名称：compile.sh
 #   创 建 者：肖飞
 #   创建日期：2023年07月25日 星期二 10时13分54秒
-#   修改日期：2024年04月26日 星期五 12时11分38秒
+#   修改日期：2024年04月26日 星期五 13时43分13秒
 #   描    述：
 #
 #================================================================
 function main() {
 	./scripts/feeds update -a
 	./scripts/feeds install -a
+
+	mv xiaofei.config .config
 	make defconfig
 	#make menuconfig
 	make clean
 	make -j$(nproc)
+	mkdir -p uploads/xiaofei
+	cp -a ./bin/targets/ramips/mt7620 uploads/xiaofei/
+
+	mv xiaofei-testing-kernel.config .config
+	make defconfig
+	#make menuconfig
+	make clean
+	make -j$(nproc)
+	mkdir -p uploads/xiaofei-testing-kernel.config
+	cp -a ./bin/targets/ramips/mt7620 uploads/xiaofei-testing-kernel.config/
 }
 
 main $@
