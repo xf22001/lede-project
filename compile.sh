7853a4c116f5d65c0ad2cd3658604fc29e825bc2
xiaofei <alibaba22001@163.com>
2024-04-26 13:46:57 +0800

    update

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@
 #   文件名称：compile.sh
 #   创 建 者：肖飞
 #   创建日期：2023年07月25日 星期二 10时13分54秒
-#   修改日期：2024年04月26日 星期五 13时43分13秒
+#   修改日期：2024年04月26日 星期五 13时46分00秒
 #   描    述：
 #
 #================================================================
@@ -14,7 +14,7 @@ function main() {
 	./scripts/feeds update -a
 	./scripts/feeds install -a
 
-	mv xiaofei.config .config
+	cp -a xiaofei.config .config
 	make defconfig
 	#make menuconfig
 	make clean
@@ -22,7 +22,7 @@ function main() {
 	mkdir -p uploads/xiaofei
 	cp -a ./bin/targets/ramips/mt7620 uploads/xiaofei/
 
-	mv xiaofei-testing-kernel.config .config
+	cp -a xiaofei-testing-kernel.config .config
 	make defconfig
 	#make menuconfig
 	make clean
