064e1a7845c1c4c41dfb33fc3503b5542c534677
xiaofei <alibaba22001@163.com>
2024-04-26 23:37:18 +0800

    update

--- compile.sh
+++ compile.sh
@@ -6,13 +6,11 @@
 #   文件名称：compile.sh
 #   创 建 者：肖飞
 #   创建日期：2023年07月25日 星期二 10时13分54秒
-#   修改日期：2024年04月26日 星期五 23时34分43秒
+#   修改日期：2024年04月26日 星期五 23时37分05秒
 #   描    述：
 #
 #================================================================
 function main() {
-	rm -rf uploads
-
 	./scripts/feeds update -a
 	./scripts/feeds install -a
 
@@ -22,6 +20,7 @@ function main() {
 	make clean
 	make -j$(nproc)
 	mkdir -p uploads/default
+	rm -rf uploads/default/mt7620
 	cp -a ./bin/targets/ramips/mt7620 uploads/default/
 
 	cp -a testing-kernel.config .config
@@ -30,6 +29,7 @@ function main() {
 	make clean
 	make -j$(nproc)
 	mkdir -p uploads/testing-kernel
+	rm -rf uploads/testing-kernel/mt7620
 	cp -a ./bin/targets/ramips/mt7620 uploads/testing-kernel/
 }
 
